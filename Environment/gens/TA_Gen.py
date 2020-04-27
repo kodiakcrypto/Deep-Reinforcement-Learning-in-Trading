e8cde265320ed4d434c6c098be02b5e01ce66599
mazmazz <mar.marcoz@outlook.com>
2020-04-27 16:29:56 -0400

    Fit minmaxscaler on training data only

--- Environment/gens/TA_Gen.py
+++ Environment/gens/TA_Gen.py
@@ -24,8 +24,12 @@ class TAStreamer(DataGenerator):
         _stock.get('dx_14')
         _stock = _stock.dropna(how='any')
 
+        # Fit minmax scaler on training data only to prevent look-forward
         min_max_scaler = preprocessing.MinMaxScaler((-1, 1))
-        np_scaled = min_max_scaler.fit_transform(_stock[['rsi_14', 'cci_14','dx_14','volume']])
+        fit_sample = _stock[['rsi_14', 'cci_14','dx_14','volume']]
+        fit_split_len = int((split-0.1)*len(fit_sample)) # deduct 0.1 for validation_split from dueling_dqn observe()
+        min_max_scaler = min_max_scaler.fit(fit_sample.iloc[:fit_split_len,:])
+        np_scaled = min_max_scaler.transform(fit_sample)
         df_normalized = pd.DataFrame(np_scaled)
         df_normalized.columns = ['rsi_14', 'cci_14','dx_14','volume']
         df_normalized['bid'] = _stock['close'].values
